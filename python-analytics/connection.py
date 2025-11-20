846e23c125510b56ccea7e1451f6192594b113bb
shomurotovazamat <shomuratov16@gmail.com>
2025-11-20 16:29:03 +0300

    env value

--- python-analytics/connection.py
+++ python-analytics/connection.py
@@ -1,12 +1,21 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
 
 try:
     conn = psycopg2.connect(
-        dbname="covid",
-        user="postgres",
-        password="1234",
-        host="localhost",
-        port="5433"
+        dbname={DB_NAME},
+        user={DB_USER},
+        password={DB_PASS},
+        host={DB_HOST},
+        port={DB_PORT}
     )
 
     cur = conn.cursor()
